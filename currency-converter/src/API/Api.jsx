2b5cc626be7eecce1423d1b3ffda8397cbd66e24
Ankita Das <daszankita23@gmail.com>
2026-07-23 16:28:11 +0530

    Currency Converter Done

--- currency-converter/src/API/Api.jsx
+++ currency-converter/src/API/Api.jsx
@@ -1,12 +1,12 @@
 import axios from"axios"; 
 const api=axios.create({
-        baseURL:"/api"
+         baseURL: "https://api.frankfurter.dev/v1"
     })
 export const fetchCurrency = async({queryKey}) =>{
     const [,amount,from,to]=queryKey;
     console.log(amount,from,to)
-    const res= await api.get(`/latest?amount=${amount}&from=${from}&to=${to}`)
+    const res= await api.get(`/latest?amount=${amount}&from=${from.toUpperCase()}&to=${to.toUpperCase()}`)
      console.log(res);
-   return res.status === 200 ? res.data : []
+   return  res.data ;
   
 }
